b14d5abe4408d44f0e000fcb720bd014cd1a5560
Erick Vivas <erickvivas415@gmail.com>
2023-05-02 15:34:59 -0700

    Second commit

--- website/food/views.py
+++ website/food/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello!')
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    return render(request,'food/index.html', context)
 
 def item(request):
     return HttpResponse('This is an item view')
