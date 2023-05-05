f9186c251aa9b18f289b65b1d01658f8f2fc1269
Erick Vivas <erickvivas415@gmail.com>
2023-05-04 19:36:47 -0700

    Seventh commit. Forms with link to add Items

--- website/food/urls.py
+++ website/food/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('item/', views.item, name='item'),
     path('<int:item_id>/', views.detail, name='detail'),
+    # Add items
+    path('add', views.create_item, name="create_item"),
 ]
--- website/food/views.py
+++ website/food/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Item
 from django.template import loader
+from .forms import ItemForm
 
 # Create your views here.
 def index(request):
@@ -21,3 +23,12 @@ def detail(request,item_id):
     }
     template = loader.get_template('food/item.html')
     return render(request,'food/item.html', context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html',{'form': form})
