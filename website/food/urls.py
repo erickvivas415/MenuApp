3aafc04abc059852e831601527bf5fe9a62f690e
Erick Vivas <erickvivas415@gmail.com>
2023-05-05 20:13:09 -0700

    Delete function

--- website/food/urls.py
+++ website/food/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('<int:item_id>/', views.detail, name='detail'),
     # Add items
     path('add', views.create_item, name="create_item"),
+    # Update items
+    path('update/<int:id>/', views.update_item, name="update_item"),
+    # Delete
+    path('delete/<int:id>/', views.delete_item, name="delete_item"),
 ]
