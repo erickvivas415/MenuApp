8f85511c07590f036bd1d3bb86a6451297335991
Erick Vivas <erickvivas415@gmail.com>
2023-05-08 06:18:46 -0700

    Alert after registration

--- website/users/views.py
+++ website/users/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Your account is created")
+            return redirect('food:index')
+    else:
+        form = UserCreationForm()
     return render(request, 'user/register.html', {'form': form})
