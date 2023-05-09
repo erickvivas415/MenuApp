6fe63f933cc660878c90c79a223b7d1b6ff40f18
Erick Vivas <erickvivas415@gmail.com>
2023-05-08 19:26:19 -0700

    Log in form and page

--- website/users/views.py
+++ website/users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request,"Your account is created")
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render(request, 'user/register.html', {'form': form})
