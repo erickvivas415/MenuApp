bd467dd61fc0352568105c6dd9c3333a1cc089b9
Erick Vivas <erickvivas415@gmail.com>
2023-05-10 19:56:50 -0700

    Restricting routes and profile page

--- website/users/views.py
+++ website/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,3 +16,8 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profilepage(request):
+
+    return render(request, 'user/profile.html')
