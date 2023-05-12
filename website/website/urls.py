3d355f8a70e801ecdde63dc9f7c0e4e808328b9b
Erick Vivas <erickvivas415@gmail.com>
2023-05-11 19:34:28 -0700

    Card with Image, location and go back button

--- website/website/urls.py
+++ website/website/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,8 @@ urlpatterns = [
     path('logout/', authentication_views.LogoutView.as_view(template_name = 'user/logout.html'), name='logout'),
     path('profile/', user_views.profilepage, name='profile'),
 ]
+
+
+
+urlpatterns += [
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
